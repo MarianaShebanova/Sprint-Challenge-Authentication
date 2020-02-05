387d43beba371581c8af07a70bcbf3034e1e93d3
MarianaShebanova <mariana.shebanova@gmail.com>
2020-02-05 12:08:51 -0800

    MVP

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,10 +3,32 @@
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
+/*module.exports = (req, res, next) => {
   if (req.session && req.session.loggedIn) {
     next();
   } else {
     res.status(401).json({ you: "shall not pass!" });
+  }
+}; */
+const jwt = require('jsonwebtoken');
+
+const { jwtSecret } = require('./secrets.js');
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        // the token is not valid
+        res.status(401).json({ you: "can't touch this!" })
+      } else {
+        req.user = { house: decodedToken.house };
+
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ you: 'shall not pass!' })
   }
 };
